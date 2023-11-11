8b3c38707324efb03c5d484faf2c91b1da1980b4
jingyuSong <68323609+sjg918@users.noreply.github.com>
2023-11-11 23:01:18 +0900

    Update annotation.py

--- annotation.py
+++ annotation.py
@@ -20,9 +20,11 @@ def drawimage(left_image, right_image, points, cur_point_idx):
         x, y, d = p[0], p[1], p[2]
         if i == cur_point_idx[0]:
             cv2.circle(left, (x, y), 5, (0, 0, 255), 2)
+            cv2.putText(left, str(d), (x, y-3), 1, 1, (0, 0, 255), 1)
             cv2.circle(right, (x-d, y), 5, (0, 0, 255), 2)
         else:
             cv2.circle(left, (x, y), 3, (0, 255, 255), 1)
+            cv2.putText(left, str(d), (x, y-3), 1, 1, (0, 255, 255), 1)
             cv2.circle(right, (x-d, y), 3, (0, 255, 255), 1)
         continue
 
@@ -51,21 +53,20 @@ def drawimage(left_image, right_image, points, cur_point_idx):
 
         left_expand = cv2.resize(left_focus, (expand_window_size, expand_window_size), interpolation=cv2.INTER_LINEAR)
         right_expand = cv2.resize(right_focus, (expand_window_size, expand_window_size), interpolation=cv2.INTER_LINEAR)
-        dif = left_expand - right_expand
 
         color = np.zeros((11, 11, 3), dtype=np.uint8)
         color[:, :, 0] = 255
         x1 = expand_window_size//2 - 5
         x2 = expand_window_size//2 + 6
-        dif = np.abs(left_expand[x1:x2, x1:x2, :] - right_expand[x1:x2, x1:x2, :]).mean()
+        dif = np.abs(left_expand[x1:x2, x1:x2, :].astype(np.float32) - right_expand[x1:x2, x1:x2, :].astype(np.float32)).mean()
         left_expand[x1:x2, x1:x2, :] = (left_expand[x1:x2, x1:x2, :].astype(np.float32) / 2 + color.astype(np.float32) / 2).astype(np.uint8)
         right_expand[x1:x2, x1:x2, :] = (right_expand[x1:x2, x1:x2, :].astype(np.float32) / 2 + color.astype(np.float32) / 2).astype(np.uint8)
         cv2.putText(left_expand, str(dif)[:6], (10, 30), 1, 2, (0, 0, 255), 1)
 
     cv2.imshow('left', left)
-    cv2.moveWindow('left', 0, 100)
+    cv2.moveWindow('left',480, 0)
     cv2.imshow('right', right)
-    cv2.moveWindow('right', 960, 100)
+    cv2.moveWindow('right', 480, 488)
     cv2.imshow('left_expand', left_expand)
     cv2.moveWindow('left_expand', 480+0, 150+540)
     cv2.imshow('right_expand', right_expand)
@@ -85,7 +86,7 @@ def mouse_event(event, x, y, flags, param):
 
 
 if __name__ == '__main__':
-    imagelist = os.listdir('./sceneflow-sample/sgm/')
+    imagelist = os.listdir('./samples/Limg/')
 
     cur_point_idx = [-1]
     cur_image_idx = 0
@@ -96,16 +97,16 @@ if __name__ == '__main__':
     cv2.namedWindow('right')
     cv2.namedWindow('right_expand')
 
-    if os.path.isfile('./sceneflow-sample/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy'):
-        points = np.load('./sceneflow-sample/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy')
+    if os.path.isfile('./samples/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy'):
+        points = np.load('./samples/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy')
         points = points.tolist()
     else:
         points = []
 
     while True:
-        left_name = './sceneflow-sample/leftimage/' + imagelist[cur_image_idx]
-        right_name = './sceneflow-sample/rightimage/' + imagelist[cur_image_idx]
-        output_name = './sceneflow-sample/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy'
+        left_name = './samples/Limg/' + imagelist[cur_image_idx]
+        right_name = './samples/Rimg/' + imagelist[cur_image_idx]
+        output_name = './samples/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy'
         
         left = cv2.imread(left_name)
         right = cv2.imread(right_name)
@@ -130,7 +131,7 @@ if __name__ == '__main__':
             if len(points) == 0:
                 cur_point_idx[0] = -1
             continue
-        elif key == 112: # p
+        elif key == 100: # d
             if len(points) == 0:
                 continue
             _, _, d = points[cur_point_idx[0]]
@@ -140,7 +141,7 @@ if __name__ == '__main__':
             if d == max_disparity:
                 d = max_disparity - 1
             points[cur_point_idx[0]][2] = d
-        elif key == 111: # o
+        elif key == 97: # a
             if len(points) == 0:
                 continue
             _, _, d = points[cur_point_idx[0]]
@@ -159,7 +160,7 @@ if __name__ == '__main__':
             if cur_point_idx[0] == len(points):
                 cur_point_idx[0] = len(points) - 1
             continue
-        elif key == 100: # d
+        elif key == 112: # p
             if saveflag == False:
                 continue
 
@@ -167,13 +168,13 @@ if __name__ == '__main__':
             if cur_image_idx == len(imagelist):
                 cur_image_idx = len(imagelist) - 1
 
-            if os.path.isfile('./sceneflow-sample/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy'):
-                points = np.load('./sceneflow-sample/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy')
+            if os.path.isfile('./samples/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy'):
+                points = np.load('./samples/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy')
                 points = points.tolist()
-                cur_point_idx = 0
+                cur_point_idx[0] = 0
             else:
                 points = []
-                cur_point_idx = -1
+                cur_point_idx[0] = -1
             continue
         elif key == 115: # s
             cur_point_idx[0] = cur_point_idx[0] - 1
@@ -184,7 +185,7 @@ if __name__ == '__main__':
             if cur_point_idx[0] == -1:
                 cur_point_idx[0] = 0
             continue
-        elif key == 97: # a
+        elif key == 111: # o
             if saveflag == False:
                 continue
 
@@ -192,13 +193,13 @@ if __name__ == '__main__':
             if cur_image_idx == -1:
                 cur_image_idx = 0
 
-            if os.path.isfile('./sceneflow-sample/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy'):
-                points = np.load('./sceneflow-sample/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy')
+            if os.path.isfile('./samples/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy'):
+                points = np.load('./samples/anno/' + imagelist[cur_image_idx].split('.')[0] + '.npy')
                 points = points.tolist()
-                cur_point_idx = 0
+                cur_point_idx[0] = 0
             else:
                 points = []
-                cur_point_idx = -1
+                cur_point_idx[0] = -1
 
             continue
         elif key == 27:
